9e440899937769e67370a10aaedf3f0223c3488b
evelinzheng <113620242+evelinzheng@users.noreply.github.com>
2023-03-15 19:35:59 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿bool Contains(string searchString, char searchChar)
+{
+
+    for (int i = 0; i < searchString.Length; i++)
+    {
+        if (searchString[i] == searchChar)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int IndexOf(string searchString, char searchChar)
+{
+    
+    for (int i = 0; i < searchString.Length; i++)
+    {
+        if (searchString[i] == searchChar)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+    int LastIndexOf(string searchString, char searchChar)
+    {
+
+
+        for (int i = searchString.Length - 1; i >= 0; i--)
+        {
+            if (searchChar == searchString[i])
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    string TrimStart(string a, char b)
+    {
+        string trimmed = a.TrimStart(b).TrimEnd(b);
+        return trimmed;
+    }
+
+
+    string SubString(string s, int start, int maxLength)
+    {
+
+        int actualLength = Math.Min(maxLength, s.Length - start);
+        string subString = s.Substring(start, actualLength);
+        return subString;
+    }
+
+
+    string Remove(string s, int startIndex, int length)
+    {
+        string firstPart = s.Substring(0, startIndex);
+        string secondPart = s.Substring(startIndex + length);
+        string newString = firstPart + secondPart;
+        return newString;
+    }
+ 
